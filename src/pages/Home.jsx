c0da713cd5543b0bdee84c60be669e33ebadb1d3
Chittox <harshchittora2001@gmail.com>
2022-06-15 00:48:59 +0530

    Creating Search Bar - Working With an API

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
 import MainPage from '../components/MainPage'
 
 function Home() {
+    const [input,setInput]=useState('')
+
+    const onInputChange=(ev)=>{
+        setInput(ev.target.value)
+        // console.log(ev.target.value)
+    }
+    const onSearch=()=>{
+        // https://api.tvmaze.com/search/shows?q=man
+    
+        fetch(`https://api.tvmaze.com/search/shows?q=${input}`).then(r=>r.json()).then(result=>{
+            console.log(result)
+        })
+    }
+const onKeyDown=(ev)=>{
+    if(ev.keyCode===13)
+    {
+        onSearch()
+    }
+    // console.log(ev.keyCode)
+}
+
   return (
-    <MainPage>this is home page !!</MainPage>
+    <MainPage>
+        <input type="text" onChange={onInputChange} onKeyDown={onKeyDown} value={input}/>
+        <button type='button' onClick={onSearch}>Search</button>
+    </MainPage>
   )
 
   }

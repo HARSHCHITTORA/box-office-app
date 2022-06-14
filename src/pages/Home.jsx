fc5be9b21c45a2adf17c37f00ec755153436c437
Chittox <harshchittora2001@gmail.com>
2022-06-15 01:06:59 +0530

    Displaying API Results - Custom Render Function

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,11 @@
 import React, { useState } from 'react'
 import MainPage from '../components/MainPage'
 
+import {apiGet} from '../misc/confg'
+
 function Home() {
     const [input,setInput]=useState('')
+    const [results,setResult]=useState(null)
 
     const onInputChange=(ev)=>{
         setInput(ev.target.value)
@@ -10,10 +13,18 @@ function Home() {
     }
     const onSearch=()=>{
         // https://api.tvmaze.com/search/shows?q=man
-    
-        fetch(`https://api.tvmaze.com/search/shows?q=${input}`).then(r=>r.json()).then(result=>{
+
+        apiGet(`/search/shows?q=${input}`).then(result=>{
+            setResult(result)
             console.log(result)
         })
+    
+        // fetch(`https://api.tvmaze.com/search/shows?q=${input}`)
+        // .then(r=>r.json())
+        // .then(result=>{
+        //     setResult(result)
+        //     console.log(result)
+        // })
     }
 const onKeyDown=(ev)=>{
     if(ev.keyCode===13)
@@ -22,11 +33,21 @@ const onKeyDown=(ev)=>{
     }
     // console.log(ev.keyCode)
 }
-
+const renderResults=()=>{
+    if(results && results.length===0)
+    {
+        return<div>No result</div>
+    }
+    if(results && results.length>0){
+            return <div>{results.map((item)=><div key={item.show.id}>{item.show.name}</div>)}</div>
+    }
+    return null;
+}
   return (
     <MainPage>
         <input type="text" onChange={onInputChange} onKeyDown={onKeyDown} value={input}/>
         <button type='button' onClick={onSearch}>Search</button>
+        {renderResults()}
     </MainPage>
   )
 
